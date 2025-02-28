c9caaf54c9714a393f15c2bd0a48d75ccf295c17
23021378 <23021378@vnu.edu.vn>
2025-02-28 21:46:22 +0700

    Add files via upload

---
+++ Lect2_Assignments/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int result = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[i] == a[j]){
+                result = result + 1;
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
